3c6768117df4dbff6a6cfdf8eb9676de32264e09
nxczos <kacper.mroz777@gmail.com>
2025-12-04 10:28:43 +0100

    remake

--- szogun.cpp
+++ szogun.cpp
@@ -1,3 +1,4 @@
+// NIE DZIAŁA to rozwiązanie
 #include<bits/stdc++.h>
  
 using namespace std;
@@ -19,59 +20,9 @@ typedef long long ll;
 
 const ll mod = 998244353;
 
-void dfs(int &v, vc<int> &l, vc<int> &r, vc<ll> &dp)
-{
-	if(v==-1) return;
-	dfs(l[v], l, r, dp);
-	dfs(r[v], l, r, dp);
-	ll lf=(l[v]==-1)? 1: ((dp[l[v]] +1)%mod);
-	ll rg=(r[v]==-1)? 1:((dp[r[v]] +1)%mod);
-	dp[v]=(lf*rg)%mod;
-	
-}
+
 void solve()
 {
-    int n;
-    cin >> n;
-
-    vc<int> a(n);
-    for(int i=0; i<n; ++i) cin >> a[i];
-
-    vc<int> par(n,-1);
-    vc<int> r(n,-1);
-    vc<int> l(n,-1);
-    stack<int> stk;
-
-    for(int i=0; i<n; ++i)
-    {
-        int last=-1;
-        while(not stk.empty() and a[stk.top()]>a[i])
-        {
-            last=stk.top();
-            stk.pop();
-        }
-
-        if(not stk.empty())
-        {
-            par[i]=stk.top();
-            r[stk.top()]=i;
-        }
-
-        if(last!=-1)
-        {
-            par[last]=i;
-			l[i]=last;
-        }
-		
-    }
-
-	int root=0;
-	for(int i=0; i<n; ++i) 
-		if(par[i]==-1) root=i;
-
-	vc<ll> dp(n,1);
-	dfs(root, l,r,dp);
-	cout << dp[root]%mod;
 }
 
 int main() 
